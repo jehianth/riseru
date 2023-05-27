b0f3636c75f5fc95fd9fc363694e4b1b1d8ccce0
jehianth <jehianbusiness@gmail.com>
2023-05-27 20:43:41 +0700

    big changes

--- script.js
+++ script.js
@@ -2,6 +2,40 @@ function goToLink(url) {
   window.location.href = url;
 }
 
+const loginButton = document.getElementById("loginButton");
+const signupButton = document.getElementById("signupButton");
+const overlay = document.getElementById("overlay");
+const popupLogin = document.getElementById("popupLogin");
+const popupSignup = document.getElementById("popupSignup");
+const switchToSignup = document.getElementById("switchToSignup");
+const switchToLogin = document.getElementById("switchToLogin");
+
+loginButton.addEventListener("click", () => {
+  overlay.style.display = "block";
+  popupLogin.classList.add("show");
+});
+
+signupButton.addEventListener("click", () => {
+  overlay.style.display = "block";
+  popupSignup.classList.add("show");
+});
+
+switchToSignup.addEventListener("click", () => {
+  popupLogin.classList.remove("show");
+  popupSignup.classList.add("show");
+});
+
+switchToLogin.addEventListener("click", () => {
+  popupSignup.classList.remove("show");
+  popupLogin.classList.add("show");
+});
+
+overlay.addEventListener("click", () => {
+  overlay.style.display = "none";
+  popupLogin.classList.remove("show");
+  popupSignup.classList.remove("show");
+});
+
 // Owlcarousel
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
